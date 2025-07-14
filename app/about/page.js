3e126b395a5074a261958761c29c7aa61a410654
fluber <fluber@gmail.com>
2025-07-14 17:00:00 +0800

    Initial NextLevel Food Project

--- app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main>
-      <h1>About This Project</h1>
-      <p>This project is a demonstration of Next.js features.</p>
-    </main>
-  );
-}
--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
-import Link from "next/link";
-import Headers from "@/components/header";
-
 export default function Home() {
-  console.log('Executing...');
   return (
     <main>
-      <Headers />
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/awesome">NextJS Is Awesome</Link></p>
-      <p><Link href="/about">About This Project</Link></p>
-      
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
